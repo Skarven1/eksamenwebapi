80d7d76b17e86ac933efa117afd79f28f6a4cae7
Skarven1 <adrian.hansen@gmail.com>
2021-04-27 01:51:09 +0200

    input and button

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,5 +2,18 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 
+function ChatApplication() {
+    return <>
+        <header>Chat App</header>
+        <main>
+            <div id="chatLog"></div>
+            <footer>
+                <form><input type='text' />
+                    <button>Submit</button>
+                </form>
+            </footer>
+        </main>
+        </>;
+}
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+ReactDOM.render(<ChatApplication/>, document.getElementById("app"));
