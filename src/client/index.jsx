2d89ae33e2eccad6c92efa46202f510c2323949b
Skarven1 <adrian.hansen@gmail.com>
2021-04-27 22:42:47 +0200

    Broadcast ws to all clients

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,37 +1,60 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 function ChatApplication() {
-    const [chatLog, setChatlog] = useState([]);
-    const [message, setMessage] = useState("")
+
+    const [chatLog, setChatLog] = useState([]);
+    const [message, setMessage] = useState("");
+    const [ws, setWs] = useState();
+
+    useEffect(() => {
+        const ws = new WebSocket("ws://localhost:3000");
+        ws.onopen = (event) => {
+            console.log("opened", event);
+        }
+        ws.onmessage = (event) => {
+            console.log("message", event);
+            setChatLog((chatLog) => [...chatLog, event.data]);
+        }
+        ws.onclose = event => {
+            console.log("close", event);
+        }
+        setWs(ws);
+    }, []);
 
     function handleSubmitChatMessage(e) {
         e.preventDefault();
-        setChatlog([...chatLog, message]);
+        ws.send(message);
+        setMessage("");
     }
 
 
-    return <>
+    return (
+        <>
         <header>
             <h1>Chat App</h1>
         </header>
         <main>
             <div id="chatLog">
-                {chatLog.map((message) => (
-                    <div>{message}</div>
+                {chatLog.map((message, index) => (
+                    <div key={index}>{message}</div>
                 ))}
             </div>
         </main>
         <footer>
             <form onSubmit={handleSubmitChatMessage}>
-                <input type='text' autoFocus={true} />
+                <input
+                    type="text"
+                    autoFocus={true}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}/>
                 <button>Submit</button>
             </form>
         </footer>
-
-        </>;
+        </>
+    );
 }
 
-ReactDOM.render(<ChatApplication/>, document.getElementById("app"));
+ReactDOM.render(<ChatApplication />, document.getElementById("app"));
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const path = require("path");
+const ws = require("ws");
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 
+const wsServer = new ws.Server({noServer: true});
+const sockets = []
+wsServer.on("connection", (socket) => {
+    sockets.push(socket)
+    socket.on("message", (message) => {
+        for (const socket of sockets) {
+            socket.send("From server:" + message);
+        }
+    });
+});
+
 
 const server = app.listen(3000, () => {
     console.log(`Started on port http://localhost:${server.address().port}`);
+    server.on("upgrade", (req, res, head) =>{
+        wsServer.handleUpgrade(req, res, head, socket => {
+            wsServer.emit("connection", socket, req);
+        });
+    });
 });
