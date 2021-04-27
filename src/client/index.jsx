ce4557a8bd9fc09ecc19b08116c79abd5d7face8
Skarven1 <adrian.hansen@gmail.com>
2021-04-27 16:31:15 +0200

    chatfunctions

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,17 +1,31 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {useState} from "react";
 
 
 function ChatApplication() {
+    const [chatLog, setChatlog] = useState([]);
+    const [message, setMessage] = useState("")
+
+    function handleSubmitChatMessage(e) {
+        e.preventDefault();
+        setChatlog([...chatLog, message]);
+    }
+
+
     return <>
         <header>
             <h1>Chat App</h1>
         </header>
         <main>
-            <div id="chatLog"></div>
+            <div id="chatLog">
+                {chatLog.map((message) => (
+                    <div>{message}</div>
+                ))}
+            </div>
         </main>
         <footer>
-            <form>
+            <form onSubmit={handleSubmitChatMessage}>
                 <input type='text' autoFocus={true} />
                 <button>Submit</button>
             </form>
