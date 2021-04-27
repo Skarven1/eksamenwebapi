4d78ab3bbe2b303e177684574acb9c8f98ab9729
Skarven1 <adrian.hansen@gmail.com>
2021-04-27 23:39:12 +0200

    extracted chatview.

---
+++ src/client/ChatView.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import {useState} from "react";
+
+export function ChatView({onSendMessage, chatLog}) {
+    const [message, setMessage] = useState("");
+
+    function handleSubmitChatMessage(e) {
+        e.preventDefault();
+        onSendMessage(message);
+        setMessage("");
+    }
+
+    return <>
+        <header>
+            <h1>Chat App</h1>
+        </header>
+        <main>
+            <div id="chatLog">
+                {chatLog.map((message, index) => (
+                    <div key={index}>{message}</div>
+                ))}
+            </div>
+        </main>
+        <footer>
+            <form onSubmit={handleSubmitChatMessage}>
+                <input
+                    type="text"
+                    autoFocus={true}
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}/>
+                <button>Submit</button>
+            </form>
+        </footer>
+    </>;
+}
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,59 +1,35 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import {useEffect, useState} from "react";
+import * as ReactDOM from "react-dom";
+import {ChatView} from "./ChatView";
 
 
 function ChatApplication() {
 
     const [chatLog, setChatLog] = useState([]);
-    const [message, setMessage] = useState("");
+
     const [ws, setWs] = useState();
 
     useEffect(() => {
         const ws = new WebSocket("ws://localhost:3000");
         ws.onopen = (event) => {
             console.log("opened", event);
-        }
+        };
         ws.onmessage = (event) => {
             console.log("message", event);
             setChatLog((chatLog) => [...chatLog, event.data]);
-        }
-        ws.onclose = event => {
+        };
+        ws.onclose = (event) => {
             console.log("close", event);
-        }
+        };
         setWs(ws);
     }, []);
 
-    function handleSubmitChatMessage(e) {
-        e.preventDefault();
-        ws.send(message);
-        setMessage("");
-    }
-
 
     return (
-        <>
-        <header>
-            <h1>Chat App</h1>
-        </header>
-        <main>
-            <div id="chatLog">
-                {chatLog.map((message, index) => (
-                    <div key={index}>{message}</div>
-                ))}
-            </div>
-        </main>
-        <footer>
-            <form onSubmit={handleSubmitChatMessage}>
-                <input
-                    type="text"
-                    autoFocus={true}
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}/>
-                <button>Submit</button>
-            </form>
-        </footer>
-        </>
+        <ChatView
+            chatLog={chatLog}
+            onSendMessage={(message) => ws.send(message)}/>
     );
 }
 
